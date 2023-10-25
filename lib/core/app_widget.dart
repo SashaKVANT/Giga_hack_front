b1e64979bc425167fbe8a194d2b1b2499a27a7a4
Sanya <buchinskii.av@phystech.edu>
2023-10-25 22:52:17 +0300

    fix: routes

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import "package:autogpt_frontend/agent_activation/presentation/agent_start_screen.dart";
 import "package:autogpt_frontend/auth/presentation/sign_in/sign_in_page.dart";
 import "package:autogpt_frontend/core/l10n/l10n.dart";
 import "package:autogpt_frontend/core/theme/color_schema.dart";
@@ -6,27 +7,31 @@ import "package:flutter/material.dart";
 import "package:flutter_localizations/flutter_localizations.dart";
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      supportedLocales: L10n.all,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
-      ],
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: lightColorScheme,
-        textTheme: appTextTheme
-      ),
-      home: const SignInPage(),
-      );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        supportedLocales: L10n.all,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightColorScheme,
+            textTheme: appTextTheme),
+        initialRoute: '/A',
+        routes: {
+          '/A': (context) => SignInPage(),
+          '/B': (context) => AgentStartScreen(),
+          // '/C': (context) => ProfilePage(),
+          // home: AgentStartScreen(),
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:autogpt_frontend/agent_activation/presentation/agent_start_screen.dart';
 import 'package:autogpt_frontend/agent_activation/presentation/screen_test.dart';
+import 'package:autogpt_frontend/core/app_widget.dart';
 import "package:autogpt_frontend/core/l10n/l10n.dart";
 import "package:autogpt_frontend/core/theme/color_schema.dart";
 import "package:autogpt_frontend/core/theme/text_theme.dart";
@@ -7,32 +8,4 @@ import "package:flutter_localizations/flutter_localizations.dart";
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const AgentStartApp());
-
-class AgentStartApp extends StatelessWidget {
-  const AgentStartApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        supportedLocales: L10n.all,
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate
-        ],
-        theme: ThemeData(
-            useMaterial3: true,
-            colorScheme: lightColorScheme,
-            textTheme: appTextTheme),
-        initialRoute: '/A',
-        routes: {
-          '/A': (context) => AgentStartScreen(),
-          '/B': (context) => HomePage(),
-          '/C': (context) => ProfilePage(),
-          // home: AgentStartScreen(),
-        });
-  }
-}
+void main() => runApp(const AppWidget());
